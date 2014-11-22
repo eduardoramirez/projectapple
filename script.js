87b0afaba4cecbf0c55d2b3da9e33ba509e94014
Shirley Quach <shquach@ieng6-202.ucsd.edu>
2014-11-22 12:56:15 -0800

    addes more needs/wants items

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
 need = {
-"Food" : 10.00,
-"Clothes": 10.00,
-"Utilities": 10.00
+"Jeans": 15.00,
+"Jacket": 10.00,
+"TShirt": 5.00,
+"Utilities": 10.00,
+"Shoes": 30.00,
+"Groceries": 25.00,
+"Utilities": 30.00,
+"Toiletries": 10.00,
+"Housing": 100.00,
+"Medicine":15.00,
+"SchoolSupplies": 5.00,
+"Transportation": 2.50,
+"Juice": 1.00
+
+
 };
 want = {
 "CDs": 16.00,
-"Console": 20.00,
-"Laptop": 25.00
+"PS4": 90.00,
+"XBOX": 50.00,
+"SNES": 25.00,
+"Popsicle": 1.00,
+"Candy": .50,
+"FastFood": 5.00,
+"Chips": 1.50,
+"Soda": 2.50,
+"Shoes": 150.00,
+"Comics": 8.50,
+"Movies": 12.00
+
 };
